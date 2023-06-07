98952f54a2b074fc7ca475ae3d9dd55c919273e4
Emmanuel <emmanuelutofa@gmail.com>
2023-06-07 21:56:06 +0200

    converted function to a non arrow function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,5 @@
-const Calculator = () => (
+function Calculator() {
+  return (
     <div className="calculator">
       <div className="display">
         <span>0</span>
@@ -24,32 +25,21 @@ const Calculator = () => (
           -
         </button>
         <button type="button">1</button>
-        <button type="button" >
-          2
-        </button>
-        <button type="button" >
-          3
-        </button>
-        <button
-          type="button"
-          className="orange-btn">
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="orange-btn">
           +
         </button>
-        <button
-          type="button"
-          className="grid-item">
+        <button type="button" className="grid-item">
           0
         </button>
-        <button type="button">
-          .
-        </button>
-        <button
-          type="button"
-          className="orange-btn"
-          >
+        <button type="button">.</button>
+        <button type="button" className="orange-btn">
           =
         </button>
       </div>
     </div>
-);
+  );
+}
+
 export default Calculator;
